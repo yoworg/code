6f01e22038e305cd3ad47f3c27b42479529b68fb
yowww <yohussin@gmail.com>
2019-09-03 17:38:49 +0100

    prefix deco

--- decorator.py
+++ decorator.py
@@ -1,14 +1,20 @@
-def hello_decorator(function):
-    def decorated_function(arg):
-        print("Decorated")
-        function(arg)
-    return decorated_function
+
+def pre_decorator(pre_arg):
+    def hello_decorator(function):
+        def decorated_function(*args, **kwargs):
+            print(pre_arg)
+            print("Decorated")
+            function(*args, **kwargs)
+        return decorated_function
+    return hello_decorator
 
 
-@hello_decorator
+@pre_decorator("Final")
 def hello(arg):
     print(arg)
 
 #hello = hello_decorator(hello)
 
 hello('test')
+
+
